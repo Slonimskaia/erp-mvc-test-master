08717edf7f95f7f16e0a6d0bfe17a48d0dd4dfe0
r4fgacon <r4fgacon@gmail.com>
2018-10-24 12:46:22 +0200

    hr

--- controller/hr_controller.py
+++ controller/hr_controller.py
@@ -2,6 +2,8 @@
 from view import terminal_view
 from model.hr import hr
 from controller import common
+from model import data_manager
+
 
 def run():
     """
@@ -12,5 +14,42 @@ def run():
     Returns:
         None
     """
+    options = ["Add", "Remove", "Update", "Oldest person", "Persons closest to average"]
+    table = data_manager.get_table_from_file("model/hr/persons.csv")
+    choice = None
+    while choice != '0':
+        choice = terminal_view.get_choice(options)
+        if choice == '1':
+            add()
+        if choice == '2':
+            remove()
+        if choice == '3':
+            update()
+        if choice == '4':
+            hr.get_oldest_person(table)
+        if choice == '5':
+            hr.get_persons_closest_to_average(table)
 
-    # your code
+        else:
+            terminal_view.print_error_message("There is no such choice.")
+
+
+def add():
+    table = data_manager.get_table_from_file("model/hr/persons.csv")
+    record = terminal_view.get_inputs(["Name:", "Year:"], "Please provide following data:")
+    hr.add(table, record)
+    data_manager.write_table_to_file("model/hr/persons.csv", table)
+
+
+def remove():
+    table = data_manager.get_table_from_file("model/hr/persons.csv")
+    id_ = terminal_view.get_inputs(["id_"], "Please provide ID you want to remove")
+    hr.remove(table, id_)
+    data_manager.write_table_to_file("model/hr/persons.csv", table)
+
+def update():
+    table = data_manager.get_table_from_file("model/hr/persons.csv")
+    id_ = terminal_view.get_inputs(["id_"], "Please provide ID you want to edit")
+    record = terminal_view.get_inputs(["Name:", "Year:"], "Please provide following data:")
+    hr.update(table, id_, record)
+    data_manager.write_table_to_file("model/hr/persons.csv", table)
--- model/hr/hr.py
+++ model/hr/hr.py
@@ -12,7 +12,6 @@ from model import data_manager
 from model import common
 
 
-
 def add(table, record):
     """
     Add new record to table
@@ -24,7 +23,9 @@ def add(table, record):
     Returns:
         list: Table with a new record
     """
-    # your code
+    record.insert(0, common.generate_random(record))
+    # common.generate_random(record)
+    table.append(record)
 
     return table
 
@@ -40,9 +41,10 @@ def remove(table, id_):
     Returns:
         list: Table without specified record.
     """
-
-    # your code
-
+    for element in table:
+        test = None
+        if element[0] == id_[0]:
+            table.remove(element)
     return table
 
 
@@ -58,9 +60,10 @@ def update(table, id_, record):
     Returns:
         list: table with updated record
     """
-
-    # your code
-
+    record.insert(0, id_[0])
+    for element in table:
+        if element[0] == id_[0]:
+            table[table.index(element)] = record[:]
     return table
 
 
@@ -78,7 +81,19 @@ def get_oldest_person(table):
         list: A list of strings (name or names if there are two more with the same value)
     """
 
-    # your code
+
+    comparision_yr = 99999
+    oldest_members = ['']
+    for element in table:
+        if int(element[2]) < comparision_yr:
+            comparision_yr = int(element[2])
+            oldest_members[0] = element[1]
+            t = 1
+        elif int(element[2]) == comparision_yr:
+            oldest_members.append(element[1])
+            
+    return oldest_members
+
 
 
 def get_persons_closest_to_average(table):
@@ -91,5 +106,18 @@ def get_persons_closest_to_average(table):
     Returns:
         list: list of strings (name or names if there are two more with the same value)
     """
-
-    # your code
+    sum = 0
+    for element in table:
+        sum += int(element[2])
+    avg = sum / len(table)
+     
+    lowest_difference = 100
+    closest_members = ['']
+    for element in table:
+        if abs(int(element[2]) - avg) < lowest_difference:
+            lowest_difference = abs(int(element[2]) - avg)
+            closest_members[0] = element[1]
+        elif int(element[2]) - avg == lowest_difference:
+            closest_members.append(element[1])
+        
+    return closest_members
